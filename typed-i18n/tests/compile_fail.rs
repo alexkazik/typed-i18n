65b4d57f66573a4599086190931d66d1ac770a9e
Alex Kazik <alex@kazik.de>
2026-04-29 17:50:19 +0200

    Reduce dependencies

--- typed-i18n/tests/compile_fail.rs
+++ typed-i18n/tests/compile_fail.rs
@@ -1,12 +1,14 @@
-// Don't run this test on nightly as it depends on the exact output, which changes on nightly a lot.
-#[rustversion::not(nightly)]
 #[test]
 fn trybuild() {
-    // Safety: The environment access only happens in single-threaded code.
-    unsafe {
-        std::env::set_var(
-            "CARGO_MANIFEST_DIR_OVERRIDE",
-            std::env::var_os("CARGO_MANIFEST_DIR").unwrap(),
-        )
-    };
+    // Don't run this test on nightly as it depends on the exact output, which changes on nightly a lot.
+    if !env!("RUSTUP_TOOLCHAIN").starts_with("nightly") {
+        // Safety: The environment access only happens in single-threaded code.
+        unsafe {
+            std::env::set_var(
+                "CARGO_MANIFEST_DIR_OVERRIDE",
+                std::env::var_os("CARGO_MANIFEST_DIR").unwrap(),
+            )
+        };
+        trybuild::TestCases::new().compile_fail("tests/compile-fail/*.rs");
+    }
 }
